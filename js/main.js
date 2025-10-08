deca87f454859506e27787a4cdf6fa449994d8a9
mostafa1873 <os8722398@gmail.com>
2025-10-08 19:45:13 +0300

    more edits

--- js/main.js
+++ js/main.js
@@ -94,7 +94,7 @@ function showDetail() {
 
 
   // DATA PRODUCT
-  
+
   const mainImage = detail.querySelector('.image img');
   const nameElement = detail.querySelector('.name');
   const descElement = detail.querySelector('.description');
@@ -156,3 +156,31 @@ function showDetail() {
     });
   }
 }
+
+/**
+ * ACCORDION
+ */
+
+const accordions = document.querySelectorAll("[data-accordion]");
+
+let lastActiveAccordion = accordions[0];
+
+const initAccordion = function (currentAccordion) {
+
+  const accordionBtn = currentAccordion.querySelector("[data-accordion-btn]");
+
+  const expandAccordion = function () {
+    if (lastActiveAccordion && lastActiveAccordion !== currentAccordion) {
+      lastActiveAccordion.classList.remove("expanded");
+    }
+
+    currentAccordion.classList.toggle("expanded");
+
+    lastActiveAccordion = currentAccordion;
+  }
+
+  accordionBtn.addEventListener("click", expandAccordion);
+
+}
+
+for (let i = 0, len = accordions.length; i < len; i++) { initAccordion(accordions[i]); }
